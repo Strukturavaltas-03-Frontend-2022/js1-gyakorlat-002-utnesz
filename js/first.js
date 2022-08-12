ac33171f2d8ff869a173664a8d53836ce83b2df1
utnesz <90829509+utnesz@users.noreply.github.com>
2022-08-12 20:05:46 +0200

    fisrt.js is ready, others are still ongoing

--- js/first.js
+++ js/first.js
@@ -4,3 +4,6 @@
 - A függvény szorozza meg a tömbelemek értékét 1.27-tel, majd pedig 
 __térjen vissza__ az elemek egész számra kerekített összegével!
 */
+
+const brutto = [10, 20, 27, 127, 1000];
+brutto.forEach(item  => Math.round(console.log(Math.round(item * 1.27))));
--- js/second.js
+++ js/second.js
@@ -3,8 +3,10 @@
 - Írj egy olyan függvénykifejezést, amely paraméterként kap egy tetszőleges 
 típusú elemeket tartalmazó tömböt és egy tetszőleges primitív értéket!
 - A függvény neve `checker` legyen!
+
 - A függvény __visszatérési értéke__ egy objektum, amely a következő 
 property-ket tartalmazza:
+
 - `exists`: Értéke true/false attól függően, hogy a második paraméterként 
 megadott érték megtalálható-e a tömbben
 - `index`: Értéke egy szám; ha a második paraméterként megadott érték 
@@ -15,3 +17,9 @@ hogy a tömb minden eleme number típusú-e vagy sem
 - `someElementsAreNumbers`: Értéke true/false attól függően, 
 hogy a tömbben van-e number típusú elem vagy sem
 */
+
+const anyArray = ['igaz', true, 42];
+
+const checker = (anyArray, 'Magrathea');
+const exists = anyArray.filter([i] === "Magrathea");
+console.log(exists);
