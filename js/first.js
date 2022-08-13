0655a680756a1f9b0a22d30dc70c92d68c679723
utnesz <90829509+utnesz@users.noreply.github.com>
2022-08-13 17:19:42 +0200

    másik verzió, test még mindig nem ok, de működik

--- js/first.js
+++ js/first.js
@@ -4,7 +4,16 @@
 - A függvény szorozza meg a tömbelemek értékét 1.27-tel, majd pedig 
 __térjen vissza__ az elemek egész számra kerekített összegével!
 */
+"Az teszt mindkét függvény esetén hibára fut, viszont mindkét függvény működik"
 
-const numeros = [10, 20, 27, 127, 1000];
-const brutto = numeros.reduce((previousValue, currentValue) => previousValue + currentValue);
-console.log(Math.round(brutto * 1.27));
+'use strict'
+
+const amountArray = [10, 20, 27, 127, 1000];
+const bruttoSum = amountArray.reduce((previousValue, currentValue) => previousValue + currentValue);
+console.log(Math.round(bruttoSum * 1.27));
+
+
+const numericArray = [10, 20, 30, 40, 50];
+const brutto = numericArray.map(item => item * 1.27);
+console.log(Math.round(brutto.reduce((previousValue, currentValue) => previousValue + currentValue)));
+
